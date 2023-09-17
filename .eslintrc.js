12e2669a58acaca478068a7109fb5d86e54d4008
Mike <mikecsomma@gmail.com>
2023-09-17 18:16:24 -0300

    Se agregó ESLint.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': ['error', 4], // Indentación con 4 espacios.
+        'linebreak-style': ['warn', 'windows'],
+        'quotes': ['warn', 'single'], // Comilla simple para strings.
+        'semi': ['error', 'always'], // Requiere ;.
+        'space-in-parens': ['warn', 'always'] // Espacio entre parentesis.
+    }
+
+};
--- src/strawberry-shortcake-api/errors/UnauthorizedError.js
+++ src/strawberry-shortcake-api/errors/UnauthorizedError.js
@@ -2,8 +2,9 @@
 
 // Error personalizado para lanzar cuando el apiKey de acceso es incorrecta.
 module.exports = class UnauthorizedError extends Error {
+    // eslint-disable-next-line no-unused-vars
     constructor( message ) {
         super( 'Para acceder a este enpoint se requiere un apiKey válida.' );
         this.name = 'UnauthorizedError';
     }
-}
+};
